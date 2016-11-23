f56cfec1d25e83967c0a6b1fb0aa01a90df5cf7a
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2016-11-23 12:07:28 -0600

    put a limit on number of definitions, converted random word first letter to uppercase, more appends

--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@ $(function() {
     type: "GET",
     url: "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
     success: function(data) {
-      var randomWord = data.word;
+      var randomWord = data.word.charAt(0).toUpperCase() + data.word.substring(1, data.word.lenght);
       $('#word').append($("<h2></h2>").text(randomWord));
       $.ajax({
         url: "https://www.googleapis.com/books/v1/volumes?q=" + randomWord,
         crossDomain: true,
         success: function(books) {
+          var shelf = $('<div class="books"></div>')
           $.each(books.items, function(i, book) {
             console.log(book.volumeInfo)
+            $(shelf).append($('<img>').attr('src', book.volumeInfo.imageLinks.smallThumbnail));
           })
+          $('#books').append(shelf);
           $.ajax({
             type: "GET",
-            url: "http://api.wordnik.com:80/v4/word.json/" + randomWord + "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+            url: "http://api.wordnik.com:80/v4/word.json/" + randomWord.toLowerCase() + "/definitions?limit=10&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
             crossDomain: true,
             success: function(definition) {
+              var definitionLine = $('<div class="definitions"></div>')
               $.each(definition, function(j, def) {
-                $('#definition').append($('<span></span>').text(def.partOfSpeech + "-  "));
-                $('#definition').append($('<p></p>').text(def.text))
+                $(definitionLine).append($('<span></span>').text((j+1) + ". " + def.partOfSpeech + ":  "));
+                $(definitionLine).append($('<p></p>').text(def.text))
+                $(definitionLine).append($('<span></span>').text("-" + def.attributionText))
+                $(definitionLine).append($('<br>'))
                 console.log(def);
               })
+              $('#definition').append(definitionLine);
             }
           })
         }
